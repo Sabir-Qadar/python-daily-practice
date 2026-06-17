46d20c976e1a73be1278eaaac1ac0afdc512d5a4
Sabir Qadar <sabirqadar@gmail.com>
2026-06-17 21:55:25 +0530

    Daily Python Practice

---
+++ escape_sequence.py
@@ -0,0 +1,7 @@
+n='hi,welcome to geek\'s'
+print(n)
+
+s1="c:\projects\name.py"
+print(s1)
+s2=r"c:\projects\name.py"   #r stands for raw string, it will ignore the escape sequence and print the string as it is
+print(s2)
---
+++ formatted_string.py
@@ -0,0 +1,9 @@
+name="Alice"
+course="Python"
+message=f"hello {name}, welcome to {course} course"
+print(message)
+
+a=20
+b=20
+sum=f"sum of {a} and {b} is {a+b}"
+print(sum)
