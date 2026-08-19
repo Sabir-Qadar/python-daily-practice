ff8492d7b62412d37611de3efcbd0e17abe1069b
Sabir Qadar <sabirqadar@gmail.com>
2026-08-19 17:18:07 +0530

    Add Q77.py: Roman to Integer

---
+++ Q77.py
@@ -0,0 +1,15 @@
+def roman_to_int(s):
+    values = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    total = 0
+    for i, ch in enumerate(s):
+        value = values[ch]
+        if i + 1 < len(s) and value < values[s[i + 1]]:
+            total -= value
+        else:
+            total += value
+    return total
+
+
+if __name__ == "__main__":
+    print(roman_to_int("MCMXCIV"))  # 1994
+    print(roman_to_int("LVIII"))    # 58
